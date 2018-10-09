6b1032cb562fd252bfe3d72f0e0166c71945b7af
darosior <darosior@gmail.com>
2018-10-09 17:35:23 +0200

    Added the get_balance function on client side

--- client/account.py
+++ client/account.py
@@ -2,7 +2,8 @@ from rpyc.utils import factory
 from rpyc.core.service import VoidService, MasterService, FakeSlaveService
 from rpyc.core.stream import SocketStream
 import rpyc
-import ssl, os, socket
+import os
+import ssl, socket
 import time
 
 class Account:
@@ -17,6 +18,7 @@ class Account:
         self.server_cert = None
         self.client_cert = None
         self.client_key = None
+        self.conn = None
         
     def connect(self, host, port=8002):
         """
@@ -37,7 +39,23 @@ class Account:
             # Wrapping it to then get the secure connnection
             s2 = ssl.wrap_socket(s, do_handshake_on_connect = False, server_side = False, ssl_version=ssl.PROTOCOL_TLSv1_2, certfile = client_cert, keyfile = client_key)
             s2.do_handshake()
-            self.connection = factory.connect_stream(SocketStream(s2), service = VoidService)
+            self.conn = factory.connect_stream(SocketStream(s2), service = VoidService)
         except ssl.SSLError as e:
             raise Exception('Could not connect to host {}:{}, getting following ssl error :\n{}'.format(host, port, e))
+    
+    def get_balance(network='all'):
+        """
+        Fetch the balance from the node.
         
+        :param where: Which network's balance should be returned. Possible values : 'all', 'bitcoin', 'lightning'.
+        
+        :return: Balance in satoshis if network == 'lightning' or network == 'bitcoin'. Else a dict with every balance.
+        """
+        balances = self.conn.root.get_balance()
+        if network == 'lightning':
+            return sum([balances['onchannel'][i] for i in balances['onchannel']])
+	    elif network == 'bitcoin':
+	        return sum([balances['onchain'][i] for i in balances['onchain']])
+	    else:
+	        return balances
+	
