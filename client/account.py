be7633575936344cadc54fa2b0f52549fbb8ae25
darosior <darosior@gmail.com>
2018-10-09 17:15:04 +0200

    Corrected Account.connect

--- client/account.py
+++ client/account.py
@@ -6,7 +6,8 @@ import ssl, os, socket
 import time
 
 class Account:
-    """The class to manage the user account.
+    """
+    The class to manage the user account.
     
     The methods to interact with the node will be placed here.
     """
@@ -29,17 +30,14 @@ class Account:
             raise
         try:
             # Creating the SSL context
-            context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cafile=server_cert)
+            context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cafile=self.server_cert)
             # Connecting the socket
-            family, socktype, proto, _, sockaddr = socket.getaddrinfo(host, port, family, socktype, proto)[0]
-            s = socket.socket(family, socktype, proto)
-            s.connect(sockaddr)
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+            s.connect((host, port))
             # Wrapping it to then get the secure connnection
             s2 = ssl.wrap_socket(s, do_handshake_on_connect = False, server_side = False, ssl_version=ssl.PROTOCOL_TLSv1_2, certfile = client_cert, keyfile = client_key)
             s2.do_handshake()
             self.connection = factory.connect_stream(SocketStream(s2), service = VoidService)
         except ssl.SSLError as e:
             raise Exception('Could not connect to host {}:{}, getting following ssl error :\n{}'.format(host, port, e))
-            
-                    
         
