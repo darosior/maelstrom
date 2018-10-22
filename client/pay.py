16857ae7f520c3b9e5f553e3b8a2a1fd0f392b7f
darosior <darosior@gmail.com>
2018-10-22 15:47:59 +0200

    Added the method to check the payment and show details

--- client/pay.py
+++ client/pay.py
@@ -9,3 +9,22 @@ class Pay(GridLayout):
         super(Pay, self).__init__(**kwargs)
         # We hide the confirmation buttons for now
         self.ids.pay.opacity = 0.0
+        self.bolt11 = ''
+        
+    def show_payment_details(self, bolt11):
+        """
+        Shows the payment details for validation.
+        :param invoice: (str) the bolt11 invoice.
+        """
+        try:
+            decoded = self.manager.app.account.decode_invoice(bolt11)
+            # Converting the time until expiration to min and msat to sat
+            decoded['expiry'] = int(decoded['expiry'])/60
+            sat = int(decoded['msatoshi'])/1000
+            usd = self.manager.app.to_usd(sat)
+            self.ids.payment_details.text = f"Pay {sat} satoshis ({usd}$) to {decoded['payee']} ?\nDescription : {decoded['description']}\nThis invoice expires in {decoded['expiry']}."
+            # We keep the valid invoice and activate the confirmation button
+            self.bolt11 = bolt11
+            self.ids.pay.opacity = 1.0
+        except:
+            self.ids.payment_details.text = 'Could not decode invoice. Maybe you should try again.'
