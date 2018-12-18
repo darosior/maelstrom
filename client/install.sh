86d5da929bc39479df8ef1a31d122ca23879a2ad
darosior@gmail.com <darosior@gmail.com>
2018-12-18 01:29:23 +0100

    Updated the install script

--- client/install.sh
+++ client/install.sh
@@ -11,11 +11,25 @@ if [ ! $INVENV ];then
     fi
 fi
 
-echo "- Installing Cython (v0.28.2)"
+printf "\033[1;33m- Installing Cython (v0.28.2)\033[0m\n"
 pip install -U Cython==0.28.2
 
-echo "- I will install the libzbar-dev package, I need privileges"
-su -c "apt update && apt install -y libzbar-dev"
+printf "\033[1;33m- I will install the libzbar-dev and cmake packages, I need privileges...\033[0m\n"
+su -c "apt update && apt install -y libzbar-dev cmake"
 
-echo "- Installing requirements"
+printf "\033[1;33m- I will install the Kivy dependencies and I need privileges once again...\033[0m\n"  
+# Copy/pasted from Kivy doc
+# Install necessary system packages
+su -c "apt install -y python3-pip build-essential git python3 python3-dev ffmpeg libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev libgstreamer1.0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good"
+
+printf "\033[1;33m- Installing requirements\033[0m\n"
 pip install -r requirements.txt
+
+printf "\033[1;33m- Installing Kivy modules\033[0m\n"
+#cd "libs/garden/"
+#printf "\033[1;33m- Building zbarcam (with OpenCV)\033[0m\n"
+#git clone "https://github.com/darosior/garden.zbarcam"
+#cd "garden.zbarcam/" && make opencv_build
+printf "Installing opencv-python"
+pip install -U opencv-python
+
