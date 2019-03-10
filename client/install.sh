c1b7d7365a94767cd4bffc30f290a90d6901b4c7
darosior <darosior@gmail.com>
2019-03-10 18:24:43 +0100

    Better pip management in install.sh

--- client/install.sh
+++ client/install.sh
@@ -3,6 +3,7 @@
 ROOTDIR=$(pwd)
 [[ "$VIRTUAL_ENV" == "" ]]; INVENV=$?
 VENVDIR="$ROOTDIR/venv"
+PIP="$VENVDIR/bin/pip"
 
 printf "\033[0;34m- Checking virtual environment.. (\"./venv\")\033[0m\n"
 if [ $INVENV -eq 0 ];then
@@ -18,10 +19,10 @@ fi
 MINOR_V=$(python -c 'import sys;print(sys.version_info[1])')
 
 printf "\033[0;34m- Upgrading pip\033[0m\n"
-pip install --upgrade pip
+$PIP install --upgrade pip
 
 printf "\033[0;34m- Installing Cython (v0.28.2)\033[0m\n"
-pip install -U Cython==0.28.2
+$PIP install -U Cython==0.28.2
 
 
 printf "\033[0;34m- Checking dependencies\033[0m\n"
@@ -31,20 +32,16 @@ source "$VENVDIR/bin/activate"
 
 
 printf "\033[0;34m- Installing requirements\033[0m\n"
-pip install -r requirements.txt
-
+$PIP install -r requirements.txt
 
 printf "\033[0;34m- Installing Kivy modules\033[0m\n"
 if [ ! -d "$ROOTDIR/libs/garden/garden.zbarcam" ];then
     cd "$ROOTDIR/libs/garden/"
     printf "\033[0;32m- Building zbarcam (and OpenCV)\033[0m\n"
     git clone "https://github.com/darosior/garden.zbarcam" && cd "garden.zbarcam/" && make opencv PYTHON_MINOR_VERSION=$MINOR_V
-    cp $ROOTDIR/libs/garden/garden.zbarcam/opencv*/build/lib/cv2.so $VENVDIR/lib/python3.$MINOR_V/site-packages/
-    cp $ROOTDIR/libs/garden/garden.zbarcam/opencv*/build/lib/python3/cv2*.so $VENVDIR/lib/python3.$MINOR_V/site-packages/
+    cp $ROOTDIR/libs/garden/garden.zbarcam/opencv-4.0.1/build/lib/cv2.so $VENVDIR/lib/python3.$MINOR_V/site-packages/
+    cp $ROOTDIR/libs/garden/garden.zbarcam/opencv-4.0.1/build/lib/python3/cv2*.so $VENVDIR/lib/python3.$MINOR_V/site-packages/
     cd "$ROOTDIR"
 fi
 printf "\033[0;34mCleaning the openCV directory\033[0m\n"
 rm -rf $ROOTDIR/libs/garden/garden.zbarcam/tmp* $ROOTDIR/libs/garden/garden.zbarcam/opencv-*
-
-printf "\033[0;34m- Installing opencv-python\033[0m\n"
-pip install -U opencv-python
